490a547973f35d2d93851ab67d6262d80be076d5
StelarCF <stelarcf@gmail.com>
2017-09-25 23:46:43 +0300

    Trialling contrib subpackage for modules that are contributted by other projects

--- src/modules/Module.java
+++ src/modules/Module.java
@@ -17,12 +17,12 @@ import java.util.logging.Logger;
  */
 public class Module extends Thread{
 
-    volatile Properties pAttributes = new Properties(); // The attributes of the current module, loaded from a file.
-    String sAttributesFile=""; // The file it is loaded from
-    String sName = ""; // The name of the instance of the module
-    ModulesManager mmManager = null; // The module manager
-    Logger logger = null; // The logger used
-    volatile boolean bReinitialize = false;
+    protected volatile Properties pAttributes = new Properties(); // The attributes of the current module, loaded from a file.
+    protected String sAttributesFile=""; // The file it is loaded from
+    protected String sName = ""; // The name of the instance of the module
+    protected ModulesManager mmManager = null; // The module manager
+    protected Logger logger = null; // The logger used
+    protected volatile boolean bReinitialize = false;
 
     public void setAttributes(Properties pSetAttributes) {
         pAttributes = pSetAttributes;
@@ -40,34 +40,4 @@ public class Module extends Thread{
 
     }
     
-//    void logSevere(String message) {
-//        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS : ");
-//        Date date = new Date();
-//        logger.severe(df.format(date) + message);
-//    }
-//    void logWarning(String message) {
-//        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS : ");
-//        Date date = new Date();
-//        logger.warning(df.format(date) + message);
-//    }
-//    void logInfo(String message) {
-//        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS : ");
-//        Date date = new Date();
-//        logger.info(df.format(date) + message);
-//    }
-//    void logConfig(String message) {
-//        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS : ");
-//        Date date = new Date();
-//        logger.config(df.format(date) + message);
-//    }
-//    void logDebug(String message) {
-//        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS : ");
-//        Date date = new Date();
-//        logger.fine(df.format(date) + message);
-//    }
-//    void logTrace(String message) {
-//        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS : ");
-//        Date date = new Date();
-//        logger.finer(df.format(date) + message);
-//    }
 }
