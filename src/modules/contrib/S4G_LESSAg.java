7602a0a478a26b157308363524a34742eec470fb
Mihai Sănduleac <mihai.sanduleac@gmail.com>
2018-04-22 13:20:45 +0300

    LESSAg update

--- src/modules/contrib/S4G_LESSAg.java
+++ src/modules/contrib/S4G_LESSAg.java
@@ -81,6 +81,15 @@ public class S4G_LESSAg extends Module {
 
                 // Here is running the main function
                 LoopFunction();
+                double P_CONS, P_PV, P_PCC, P_BAT;
+                //P_CONS = Double.parseDouble((String) pDataSet.get(sPrefix + "SMX/Values/P_CONS/value"));
+                P_PV = Double.parseDouble((String) pDataSet.get(sPrefix + "SMX/Values/P_PV/value"));
+                P_PCC = Double.parseDouble((String) pDataSet.get(sPrefix + "SMX/Values/P_PCC/value"));
+                P_BAT = Double.parseDouble((String) pDataSet.get(sPrefix + "SMX/Values/P_BAT/value"));
+                P_CONS = -P_PCC - P_PV - P_BAT;
+                if(P_PCC<0) P_BAT=P_BAT+P_PCC;
+                System.out.println("Hello: P_CONS="+P_CONS+"\tP_PV="+P_PV+"\tP_PCC="+P_PCC+"\tP_BAT="+P_BAT);
+                pDataSet.put(sPrefix+"SMX/Values/P_BAT/value", Double2DecPointStr(P_BAT,2));
 
             } catch (Exception e) {
                 if (Debug == 1) {
@@ -113,5 +122,24 @@ public class S4G_LESSAg extends Module {
     public double ddt = 0.0;
     public long lDelay = 0;
 
+    public String Double2DecPointStr(double dVal, int iDecimals) {
+        String sResult = " ";
+        int iLen, iNoOfIntDigits;
+        try {
+            sResult = Double.toString(dVal);
+            iLen = sResult.length();
+            iNoOfIntDigits = sResult.indexOf(".");
+            if (iLen - iNoOfIntDigits - 1 > iDecimals) {
+                if (iDecimals < 1) {
+                    sResult = sResult.substring(0, iNoOfIntDigits + iDecimals);
+                } else {
+                    sResult = sResult.substring(0, iNoOfIntDigits + iDecimals + 1);
+                }
+            }
 
+        } catch (Exception e) {
+
+        }
+        return sResult;
+    }
 }
